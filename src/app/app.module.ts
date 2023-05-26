25a6f6963ca430d7f69018ed8ee6f98c3788008e
Angélique <angel.belg@hotmail.fr>
2023-05-26 10:32:55 +0200

    maj module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyDirectiveDirective } from './directives/my-directive.directive';
+import { SqrtPipe } from './pipes/sqrt.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateurComponent,
+    MyDirectiveDirective,
+    SqrtPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
