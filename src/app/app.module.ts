2b526934b704d831ef857625d91ede2041984991
margsalanne <margaux.salanne@gmail.com>
2023-05-26 11:09:08 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+import { RoleComponent } from './role/role.component';
+import { AvisComponent } from './avis/avis.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { PrestationComponent } from './prestation/prestation.component';
+import { SalonComponent } from './salon/salon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UtilisateurComponent
+    UtilisateurComponent,
+    RoleComponent,
+    AvisComponent,
+    ReservationComponent,
+    PrestationComponent,
+    SalonComponent
   ],
   imports: [
     BrowserModule,
--- src/app/utilisateur/utilisateur.component.ts
+++ src/app/utilisateur/utilisateur.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-utilisateur',
   templateUrl: './utilisateur.component.html',
   styleUrls: ['./utilisateur.component.css']
 })
-export class UtilisateurComponent {
-
+export class UtilisateurComponent implements OnInit{
+  // Déclaration d'un tableau d'utilisateurs
+  // ! : le tableau n'est pas initialisé
+  users!:any[];
+  user:Utilisateur=new Utilisateur(); // any : n'importe quel type de données
+  // DI : par constructeur  
+  constructor(private utilisateurService:UtilisateurService){
+  }
+  ngOnInit(): void {
+    this.findAllUtilisateurs();
+  }
+  findAllUtilisateurs(){
+    // utilisation expression lambda 
+    // data sont les données qui sont dans le cache du navigateur
+    this.utilisateurService.findAll().subscribe(data => {this.users = data});
+  }
+  saveUtilisateur(){
+    this.utilisateurService.save(this.user).subscribe(
+      () => {
+        // MAJ liste des utilisateurs
+        this.findAllUtilisateurs();
+        // Vider le formulaire
+        this.user = new Utilisateur();
+      }
+    )
+  }
+  deleteUtilisateur(id:number){
+    this.utilisateurService.delete(id).subscribe(
+      () => {
+        this.findAllUtilisateurs();
+      }
+    )
+  }
 }
